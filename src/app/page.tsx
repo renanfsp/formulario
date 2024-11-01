e62a2caa67342ba6e56e70d8a025cdb4db6f6a20
Renan <renanplays@outlook.com.br>
2024-11-01 18:59:01 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+// type PerguntaSecreta = {
+//   texto: string
+//   resposta: string[]
+//   perguntaSecreta?: {
+//     gatilho: string[]
+//     pergunta: PerguntaSecreta
+//   }
+// }
+
+// type Pergunta = {
+//   id: number
+//   texto: string
+//   resposta: string[]
+//   perguntaSecreta: {
+//     gatilho: string[],
+//     perguntaSecreta: PerguntaSecreta
+//   }
+// }
+
+export default function Home() {
+  // const handleResposta = () => {}
+  // const handleFormulario = () => {}
+
+  return (
+    <div>
+      <h1>Responda nosso questionario</h1>
+      <button>Confirmar</button>
+    </div>
+  )
+}
